d455ff6b158be5757ed3f691e689b9c8596de6cd
ccycc88 <ccycc88@163.com>
2018-08-13 17:02:53 +0800

    impl task

---
+++ com/ccycc/thread/TestTask.py
@@ -0,0 +1,18 @@
+'''
+
+@author: v-chenyangchao-os
+'''
+from com.ccycc.thread.AbsTractTask import AbsTractTask
+
+class TestTask(AbsTractTask):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def execute(self):
+        print('hello') 
